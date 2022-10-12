3ec629771d34f6af78a74fffb6bfade6e10dfbdf
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2022-10-12 11:05:37 -0700

    dark theme

--- index.js
+++ index.js
@@ -2,30 +2,40 @@
 //service_5iioa8u
 //16_fH1SOUJvHOc-Ju
 let isModalOpen = false;
+let contrastToggle = false;
 
-function toggleContrast 
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
 
 
 function contact(event) {
     event.preventDefault();
-    const loading = document.querySelector('.modal__overlay--loading');
-    const success = document.querySelector('.modal__overlay--success');
+    const loading = document.querySelector(".modal__overlay--loading");
+    const success = document.querySelector(".modal__overlay--success");
     loading.classList += " modal__overlay--visible";
     emailjs
        .sendForm(
-           'service_5iioa8u',
-           'template_ziw5jhs',
+           "service_5iioa8u",
+           "template_ziw5jhs",
            event.target,
-           '16_fH1SOUJvHOc-Ju',
+           "16_fH1SOUJvHOc-Ju",
     ).then(() => {
         loading.classList.remove("modal__overlay--visible");
         success.classList += " modal__overlay--visible";
-    }).catch(() => {
+    })
+    .catch(() => {
         loading.classList.remove("modal__overlay--visible");
         alert(
             "The email service is temporary unavailable. Please contact me directly on paquito1981@outlook.com"
         );
-    })
+    });
 }
 
 function toggleModal() {

d62337c3cef843db1800a0cc1062db4327523fba
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2022-10-12 15:45:23 -0700

    shape

--- index.js
+++ index.js
@@ -3,6 +3,19 @@
 //16_fH1SOUJvHOc-Ju
 let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function movebackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px) rotate(${x * boolInt * 10}deg)`
+    }
+}
 
 function toggleContrast() {
     contrastToggle = !contrastToggle;
